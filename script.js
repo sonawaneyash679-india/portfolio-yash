1b577355c8baebdbb403afb3847e73dee3736f2f
Bhushan Sonawane <sonawaneyash679@gmail.com>
2026-07-08 15:41:06 +0530

    script.js

--- script.js
+++ script.js
@@ -1,61 +1,81 @@
-// Portfolio Loaded
-console.log("Bhushan Portfolio Loaded Successfully!");
+console.log("Bhushan AIML Portfolio Loaded Successfully!");
 
 
 // Smooth scrolling
+
 document.querySelectorAll('a[href^="#"]').forEach(link => {
 
     link.addEventListener("click", function(e){
 
         e.preventDefault();
 
-        const target = document.querySelector(this.getAttribute("href"));
+        let target = document.querySelector(
+            this.getAttribute("href")
+        );
+
 
         if(target){
+
             target.scrollIntoView({
-                behavior: "smooth"
+                behavior:"smooth"
             });
+
         }
 
     });
 
 });
+
+
 
 
 // Scroll reveal animation
+
 const sections = document.querySelectorAll("section");
 
 
-window.addEventListener("scroll", () => {
-
-    sections.forEach(section => {
-
-        const sectionTop = section.getBoundingClientRect().top;
-
-        const screenPosition = window.innerHeight / 1.3;
+window.addEventListener("scroll",()=>{
 
 
-        if(sectionTop < screenPosition){
+    sections.forEach(section=>{
 
-            section.classList.add("show");
+
+        let position = section.getBoundingClientRect().top;
+
+
+        let screenHeight = window.innerHeight;
+
+
+        if(position < screenHeight - 100){
+
+            section.style.opacity="1";
+
+            section.style.transform="translateY(0)";
 
         }
 
+
     });
+
 
 });
 
 
-// Button click message
-const buttons = document.querySelectorAll(".btn");
 
 
-buttons.forEach(button => {
+// Project card click message
 
-    button.addEventListener("click", () => {
+const projects = document.querySelectorAll(".project");
 
-        console.log("Button clicked");
+
+projects.forEach(card=>{
+
+
+    card.addEventListener("mouseenter",()=>{
+
+        console.log("Project viewed");
 
     });
+
 
 });
